04260f29b6066e18ea30e7ba6fc33e39749dc725
Vivek Sharma <ranusharma14112003@gmail.com>
2025-09-29 19:54:32 +0530

    Updated endpoints

--- src/PublicPages/Home.jsx
+++ src/PublicPages/Home.jsx
@@ -7,53 +7,177 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import SimpleSlider from "../components/Slider";
 import AboutUs from "../components/Aboutus";
-
+import { viewAllProductsForUsers } from "../Endpoints/endpoints";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 const Home = () => {
-  // const role = useRole();
-  // console.log(role);
+  const [totalPage, setTotalPage] = useState();
+  const [page, setPage] = useState(1);
+  const search = useSelector((state) => state?.customers?.search);
+  const queryClient = useQueryClient();
+  queryClient.invalidateQueries({ queryKey: ["product"] });
 
+  const getProducts = async () => {
+    const res = await viewAllProductsForUsers(page, search);
+    setTotalPage(res?.data?.totalCount);
 
-  // let chars = ['Bcs', 'Bcs', 'A', 'C', 'B'];
-  // let uniqueChars = [...new Set(chars)];
-  // console.log(uniqueChars); // Output: ['A', 'B', 'C']
-  // const func =()=>{
-  //   const c = window.document.getElementById("test")
-  //  console.log(c.value," >>>>>>>>?????????????????");
-     
-  //  const res = window.document.getElementById("demo")
-  
-  // res.value = c.value
-  // console.log(res.value,"???????/");
-  // }
+    return res.data;
+  };
+  const { data } = useQuery({
+    queryKey: ["product", page],
+    queryFn: getProducts,
+  });
 
-  // const myFunc = ()=>{
-  //   window.document.getElementById("demo").innerHTML = "You are a human";
-  //   console.log(myFunc());
-    
-  //   return true
-  // }
-  // if (myFunc == true) {
-    
-  // }else{
-  //   document.getElementsByClassName("kk").innerHTML = "Robot";
-  // }
-  
-  // const isHuman = "" 
-  
   return (
     <>
       <Header />
       <div className=" container-fluid  p-0">
         <SimpleSlider />
+
+      </div>
+      <div className="container p-0 mt-3">
+        <div className="row">
+          {data &&
+            data?.data?.map((product) => {
+              return (
+                <>
+                  <div className="col-md-6 col-lg-4 col-sm-12">
+                    {/* <button> */}
+                  
+                    <div className="product-cardd">
+                    <Link
+                      to={`/customer/view-product/${product._id}`}
+                      style={{ textDecoration: "none", color: "inherit" }}
+                    >
+                      <div className="badge">{product?.brand}</div>
+                    
+                      <div className="product-tumb">
+                     
+                        <img
+                          src={`http://localhost:5000/api/${product.image}`}
+                          alt={product?.name}
+                        />
+                      </div>
+                    </Link>
+                      <div className="product-details">
+                      <Link
+                      to={`/customer/view-product/${product._id}`}
+                      style={{ textDecoration: "none", color: "inherit" }}
+                    >
+                        <span className="product-catagory  mb-2">
+                          {product?.category}
+                        </span>
+                        <h4>
+                          <a className="mb-2" href="">
+                            {product?.name.slice(0,18)}...
+                          </a>
+                        </h4>
+                       
+                        <p className="mb-1"> {product?.features.slice(0,100)}...</p>
+                          </Link>
+                        <div className="product-bottom-details">
+                          <div className="product-price p-0 m-0">
+                            Price: ₹{product?.price}
+                          </div>
+                          {/* */}
+                          <div className="product-links">
+                            <button
+                              type="button"
+                              onClick={() => {
+                                if (product?.wishItem) {
+                                  removeFromLiked.mutate({
+                                    productId: product?._id,
+                                  });
+                                } else {
+                                  isLikeMutaion.mutate({
+                                    productId: product?._id,
+                                  });
+                                }
+                              }}
+                            >
+                              {product?.wishItem ? (
+                                <i
+                                 className="fa-solid fa-heart fs-4"
+                                  style={{ color: "red" }}
+                                ></i>
+                              ) : (
+                                <i
+                                 className="fa-regular fa-heart "
+                                  style={{ color: "black" }}
+                                ></i>
+                              )}
+                            </button>
+                          </div>
+                        </div>
+                      </div>
+                 
+                      {product?.stock == 0 ? (
+                        <div className="out-of-stock">
+                          <p className="text-center p-3 bg-danger">
+                            Out of Stock
+                          </p>
+                        </div>
+                      ) : (
+                        <div className="p-2 d-flex justify-content-evenly">
+                          <button
+                            type="button"
+                            className="btnu py-2 px-3"
+                            onClick={() => {
+                              mutaionForAddToCart.mutate({
+                                productId: product?._id,
+                              });
+                            }}
+                          >
+                            Add To Cart
+                            {/* <FaCartPlus /> */}
+                          </button>
+                          <button
+                            type="button"
+                            className="btnu py-2"
+                            onClick={() =>
+                              navigate("/customer/paynow", {
+                                state: { products: [product] },
+                              })
+                            }
+                          >
+                            Buy Now
+                          </button>
+                        </div>
+                      )}
+                    </div>
+                   
+                   
+                  </div>
+                 
+                </>
+              );
+            })}
+        </div>
+      {totalPage/10 > 1 ?
+       ( <ReactPaginate
+          breakLabel="..."
+          nextLabel=" >"
+          onPageChange={(e) => setPage(e.selected + 1)}
+          pageRangeDisplayed={totalPage}
+          pageCount={totalPage/10}
+          previousLabel="< "
+          renderOnZeroPageCount={null}
+          containerClassName="pagination"
+          pageClassName="page-item"
+          pageLinkClassName="page-link"
+          previousClassName="previous-item"
+          previousLinkClassName="previous-link"
+          nextClassName="next-item"
+          nextLinkClassName="next-link"
+          breakClassName="break-item"
+          breakLinkClassName="break-link"
+          activeClassName="selected"
+          disabledClassName="disabled"
+        /> 
+      ) : ""
+         } 
       </div>
       <AboutUs />
- {/* <div>
-  <input type="text " placeholder="type Name" id="test" className="kk" />
-  <p id="test1"></p>
-  <button type=" button" onClick={()=>func() } onkeydown={()=>myFunc()} className="btnu">Click it</button>
-  <input type="text " id="demo"  className="kk"/>
-  <p id="demo1"></p>
- </div> */}
+
       <Footer />
     </>
   );
